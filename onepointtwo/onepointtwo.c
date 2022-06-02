e8bbfbb79633f17c3631340a3d18ec5e8de6c851
richa130 <richabavadekar@me.com>
2022-06-01 23:51:32 -0700

    implemented reverse() and created Makefile, 1.2 done

---
+++ onepointtwo/onepointtwo.c
@@ -0,0 +1,25 @@
+/* 1.2 - Implement a function void reverse(char *str) in C/C++ which reverses a null terminated string */
+
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char *str){
+    int len = strlen(str); // strlen does NOT include the null terminator
+
+    for(int i = 0; i < len/2; i++){
+	char tmp = str[i];
+	str[i] = str[len - 1 - i];
+	str[len - 1 - i] = tmp;
+    }
+}
+
+// main takes CLAs, reverses them, then prints them 
+int main(int argc, char **argv){
+    while(*++argv){
+	reverse(*argv);
+	printf("%s ",*argv);
+    }
+    printf("\n");
+
+    return 0;
+}
